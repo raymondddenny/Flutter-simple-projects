bb5156eb86c2ce5755d0da99d6f124b8ed3757ba
Denny Raymond <rrettob@gmail.com>
2020-04-10 18:22:36 +0800

    8 Ball Decision app

---
+++ magic-8-ball-flutter/lib/main.dart
@@ -0,0 +1,48 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: BallApp(),
+      ),
+    );
+
+class BallApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Ask Me Anything"),
+        backgroundColor: Colors.blue.shade900,
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue,
+      child: FlatButton(
+        child: Center(
+          child: Image.asset('images/ball$ballNumber.png'),
+        ),
+        onPressed: () {
+          print('ball number = $ballNumber');
+          setState(() {
+            ballNumber = Random().nextInt(5) + 1;
+          });
+        },
+      ),
+    );
+  }
+}
