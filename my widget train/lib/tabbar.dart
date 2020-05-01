f27d0f778da9bcb2b81603a4b7d79bf9f5ffb037
Denny Raymond <rrettob@gmail.com>
2020-05-01 20:06:16 +0800

    custom tab bar

--- my widget train/lib/tabbar.dart
+++ my widget train/lib/tabbar.dart
@@ -1,48 +1,45 @@
 import 'package:flutter/material.dart';
 
-// for this tabbar widget, don't know why the icon with text only can put in 1 tab only
-// if there are another icon with text it's show bug
-
 class TabbarPage extends StatefulWidget {
   @override
   _TabbarPageState createState() => _TabbarPageState();
 }
 
 class _TabbarPageState extends State<TabbarPage> {
+  TabBar tabb = TabBar(
+    // to change the tab bar indicator color
+    indicatorColor: Colors.blue,
+    tabs: <Widget>[
+      Tab(
+        icon: Icon(Icons.home),
+        text: "Home",
+      ),
+      Tab(
+        icon: Icon(Icons.settings),
+        text: "Settings",
+      ),
+      Tab(
+        icon: Icon(Icons.comment),
+        text: "Comments",
+      ),
+    ],
+  );
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: 4,
+      length: 3,
       child: Scaffold(
         appBar: AppBar(
           title: Text('Tab bar widget'),
-          bottom: TabBar(
-            tabs: <Widget>[
-              Tab(
-                icon: Icon(Icons.home),
-                text: "Home",
-              ),
-              Tab(
-                child: Image(
-                  image: NetworkImage(
-                      "http://clipart-library.com/new_gallery/37-373099_chick-yellow-yellowchick-tumblr-aesthetic-cute-kawaii-cute.png"),
-                ),
-              ),
-              Tab(
-                child: Icon(Icons.settings),
-              ),
-              Tab(
-                text: "Comments",
-              ),
-            ],
-          ),
+          bottom: PreferredSize(
+              //to set the tab bar color, we need to set container to bottom : then wrap with preferedSize widget
+              //to get the size of our tab bar
+              preferredSize: Size.fromHeight(tabb.preferredSize.height),
+              child: Container(color: Colors.green, child: tabb)),
         ),
         body: TabBarView(children: <Widget>[
           Center(
             child: Text('Tab Home'),
-          ),
-          Center(
-            child: Text('Tab Cute'),
           ),
           Center(
             child: Text('Tab Settings'),
